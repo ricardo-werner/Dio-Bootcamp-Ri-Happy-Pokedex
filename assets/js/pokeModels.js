26ff6f594647e838e0ca04e96f0bd51595d1441a
Ricardo Werner <wernerdive@gmail.com>
2024-11-23 22:00:49 -0300

    feat: add novos detalhes no constructor para o modal de detalhes

--- assets/js/pokeModels.js
+++ assets/js/pokeModels.js
@@ -1,10 +1,13 @@
 export class Pokemon {
 
-  constructor(number, name, type, types, image) {
+  constructor(number, name, type, types, image, abilities, heigth, weight) {
     this.number = number;
     this.name = name;
     this.type = type;
     this.types = types || [];
     this.image = image;
+    this.abilities = [];
+    this.height = heigth;
+    this.weight = weight;
   }
 }
